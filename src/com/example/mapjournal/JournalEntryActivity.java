9da4b24bf6df8d684fa08e8c4e36091d648a75a7
Eric Zeng <ericzeng2@gmail.com>
2012-07-26 15:42:45 -0700

    Minor edit to text editor activity

--- src/com/example/mapjournal/JournalEntryActivity.java
+++ src/com/example/mapjournal/JournalEntryActivity.java
@@ -6,7 +6,6 @@ package com.example.mapjournal;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
 
@@ -17,7 +16,7 @@ public class JournalEntryActivity extends Activity {
 	private String note;
 	private DBOpenHelper db;
 	
-	
+	// Gets text associate with a point and puts it in the text editor.
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_journal_entry);
@@ -31,22 +30,22 @@ public class JournalEntryActivity extends Activity {
 		db = new DBOpenHelper(this);
 		note = db.getNote(pointID);
 		
-		
+		// Put current text into text box
 		editor = (EditText) findViewById(R.id.text_editor);
-		
 		editor.setText(note);
 		
 	}
 	
+	// Adds changes to text to the database. 
 	public void submitEdit(){
 		note = editor.getText().toString();
 		db.updateNote(pointID, note);
 		gotoMap();
 	}
 	
-
+	// Returns to the map activity.
 	public void gotoMap(){
-		Intent intent = new Intent(JournalEntryActivity.this, MapJournalMapActivity.class);
+		Intent intent = new Intent(this, MapJournalMapActivity.class);
 		startActivity(intent);		
 	}	
 	
