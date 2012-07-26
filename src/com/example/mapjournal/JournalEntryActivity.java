84232a26aac15021fb5aca8f2e1892f10d9eafda
Eric Zeng <ericzeng2@gmail.com>
2012-07-26 15:06:06 -0700

    Implementing text editor (WIP)

--- src/com/example/mapjournal/JournalEntryActivity.java
+++ src/com/example/mapjournal/JournalEntryActivity.java
@@ -1,10 +1,42 @@
+// Eric Zeng
+// Retrieves information about a selected map point and allows the
+// user to edit it.
+
 package com.example.mapjournal;
 
 import android.app.Activity;
-
-
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.EditText;
 
 public class JournalEntryActivity extends Activity {
+	
+	private long pointID;
+	private EditText editor;
+	private String note;
+	
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_journal_entry);
+		
+		// Retrieve point from map
+		Intent intent = getIntent();
+		pointID = intent.getLongExtra(ID, pointID);
+		
+		// Retrieve current text of point from database
+		DBOpenHelper db = new DBOpenHelper(this);
+		note = db.getNote(pointID);
+		
+		
+		editor = (EditText) findViewById(R.id.text_editor);
+		
+		
+	}
+	
+	
+	
 	
 	
 }
