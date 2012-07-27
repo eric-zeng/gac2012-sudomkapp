c1de29c706b8a82160b7eabd825de197c27ddd01
Eric Zeng <ericzeng2@gmail.com>
2012-07-26 21:25:04 -0700

    Added delete functionality for previous trip list, changed strings for the action bar on each page. Major changes!

--- src/com/example/mapjournal/PreviousTripsActivity.java
+++ src/com/example/mapjournal/PreviousTripsActivity.java
@@ -2,11 +2,14 @@ package com.example.mapjournal;
 
 
 import java.util.ArrayList;
+
+import android.content.ClipData.Item;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,34 +18,42 @@ import android.widget.Toast;
 public class PreviousTripsActivity extends ListActivity 
 {
 	private DBOpenHelper db;
+	private boolean delete;
+	private ArrayList<String> tripNames;
     public static final String PREFS_NAME = "PrefsFile";
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         db = new DBOpenHelper(this);
-        ArrayList<String> tripNames = new ArrayList<String>();
         tripNames = db.getAllTrips();
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tripNames));
+  
+        delete = false;
         
     }
 
     @Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-	
-		super.onListItemClick(l, v, position, id);
+    	super.onListItemClick(l, v, position, id);
 		Object o = this.getListAdapter().getItem(position);
 		String keyword = o.toString();
-		
-		SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString("current", keyword);
-		editor.commit();
-		
-		Intent intent = new Intent(this, MapJournalMapActivity.class);
-		startActivity(intent);
-		
-		Toast.makeText(this, "You Have Selected: " + keyword, Toast.LENGTH_LONG).show();
+    	
+    	if(delete){
+    		tripNames.remove(keyword);
+    		db.deleteTrip(keyword);
+    		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tripNames));
+    	}else{
+			SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+			SharedPreferences.Editor editor = prefs.edit();
+			editor.putString("current", keyword);
+			editor.commit();
+			
+			Intent intent = new Intent(this, MapJournalMapActivity.class);
+			startActivity(intent);
+			
+			Toast.makeText(this, "You Have Selected: " + keyword, Toast.LENGTH_LONG).show();
+    	}
 	}
 
 	@Override
@@ -50,4 +61,15 @@ public class PreviousTripsActivity extends ListActivity
         getMenuInflater().inflate(R.menu.activity_previous_trips, menu);
         return true;
     }
+	
+	public void toggleDelete(MenuItem button){
+		if(delete){
+			delete = false;
+			button.setTitle("Delete Trips");
+		}else{
+			delete = true;
+			button.setTitle("Done");
+		}
+	
+	}
 }
