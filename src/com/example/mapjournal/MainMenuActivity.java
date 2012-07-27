f9b697ffe3221a14a8b1d8660227f52e034be532
Eric Zeng <ericzeng2@gmail.com>
2012-07-26 22:13:08 -0700

    What's brown and sticky? A stick! Commented main menu, removed useless settings xml file for main menu.

--- src/com/example/mapjournal/MainMenuActivity.java
+++ src/com/example/mapjournal/MainMenuActivity.java
@@ -1,64 +1,55 @@
 package com.example.mapjournal;
-//Created by Eric
-//Changed by Leo
-import java.util.prefs.Preferences;
 
-
-import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
+/**
+ * The main menu and start page for the app. Either switches to a new trip, resumes the
+ * most recent trip, or allows you to view and resume older trips. 
+ * @author Eric
+ *
+ */
+
 public class MainMenuActivity extends ListActivity {
 
-	private String currentTrip;
     public static final String PREFS_NAME = "PrefsFile";
-	
+    
+    /**
+     * Shows three options to continue: new trip, resume trip, or view previous trips.
+     */
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        //setContentView(R.layout.activity_main_menu);
+        // Creates an array containing the available options and uses the list adapter to display them. 
         String[] tripNames = new String[]{"New Trip", "Resume Trip", "Previous Trips"};
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tripNames));
         ListView lv = getListView();
         
-        // listening to single list item on click
+        // Listens for a click on the list
         lv.setOnItemClickListener(new OnItemClickListener() {
-          public void onItemClick(AdapterView<?> parent, View view,
-              int position, long id) {
+        	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
  
-              // selected item
-              String product = ((TextView) view).getText().toString();
-              if(product == "New Trip"){
-            	  Intent intent = new Intent(getApplicationContext(), NewTripActivity.class);
-                  startActivity(intent);
-              }
-              else if(product == "Resume Trip"){
-            	  Intent intent = new Intent(getApplicationContext(), MapJournalMapActivity.class);
-              	  startActivity(intent);
-              }
-              else{
+        		// Pulls the text from the option chosen and uses it to decide which activity to switch to. 
+        		String menuChoice = ((TextView) view).getText().toString();
+        		if(menuChoice == "New Trip"){
+        			Intent intent = new Intent(getApplicationContext(), NewTripActivity.class);
+        			startActivity(intent);
+        		} else if(menuChoice == "Resume Trip") {
+        			Intent intent = new Intent(getApplicationContext(), MapJournalMapActivity.class);
+              	  	startActivity(intent);
+        		} else {
             	  Intent intent = new Intent(getApplicationContext(), PreviousTripsActivity.class);
                   startActivity(intent);
-              }
-              // sending data to new activity
-              
-          }
+        		}  
+        	}
         });
-    }
-       
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
-        return true;
     }
 }
