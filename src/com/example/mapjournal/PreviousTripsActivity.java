2c829afa962409b8be2d368479f42528e70ed6ba
Eric Zeng <ericzeng2@gmail.com>
2012-07-26 17:46:39 -0700

    Remove commented out code, extra imports, etc in PreviousTripsActivity.

--- src/com/example/mapjournal/PreviousTripsActivity.java
+++ src/com/example/mapjournal/PreviousTripsActivity.java
@@ -2,11 +2,8 @@ package com.example.mapjournal;
 
 
 import java.util.ArrayList;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.widget.TextView;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.view.Menu;
@@ -17,28 +14,22 @@ import android.widget.Toast;
 
 public class PreviousTripsActivity extends ListActivity 
 {
-	DBOpenHelper db = new DBOpenHelper(this);
+	private DBOpenHelper db;
     public static final String PREFS_NAME = "PrefsFile";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_previous_trips);
+        db = new DBOpenHelper(this);
         ArrayList<String> tripNames = new ArrayList<String>();
         tripNames = db.getAllTrips();
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tripNames));
         
-//        TextView txtProduct = (TextView) findViewById(R.id.product_label);
-//        
-//        Intent i = getIntent();
-//        // getting attached intent data
-//        String product = i.getStringExtra("product");
-//        // displaying selected product name
-//        txtProduct.setText(product);
     }
 
     @Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO Auto-generated method stub
+	
 		super.onListItemClick(l, v, position, id);
 		Object o = this.getListAdapter().getItem(position);
 		String keyword = o.toString();
